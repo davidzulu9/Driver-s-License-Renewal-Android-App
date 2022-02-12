36300bc4a358abd9d573464e090e8f89bac5a446
Volk <zuludavid40@gmail.com>
2022-02-12 21:06:06 +0200

    Decided to finally make my first git commit

---
+++ app/src/main/java/com/example/driverlicenserenewal/HomeActivity.java
@@ -0,0 +1,6 @@
+package com.example.driverlicenserenewal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity{
+}
---
+++ app/src/main/java/com/example/driverlicenserenewal/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.driverlicenserenewal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout nameText, passwordText, questionText;
+    TextView signUpText;
+    MaterialButton loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Binding the widgets
+        loginButton = findViewById(R.id.loginButton);
+        signUpText = findViewById(R.id.signUpText);
+
+        //adding functionality
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
+}
